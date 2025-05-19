d310bbe01fb1b649822b97c32586361363ffbfb2
julianafailla <faillajuliana8@gmail.com>
2025-05-19 20:39:09 -0300

    Revert "Merge branch 'ControladorUsuario' into develop"
    This reverts commit eccb4ab51300fb1cad2dc1494a843fb5fdd9f77e, reversing
    changes made to c64b0ad28213f0dd59187626f6382a9f7f7bef84.

--- SistemaGestionUniversitario/Entidades/DTOs/UsuarioDTO.cs
+++ SistemaGestionUniversitario/Entidades/DTOs/UsuarioDTO.cs
@@ -8,7 +8,6 @@ namespace Entidades.DTOs
         public string DNI { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
-        public string Password { get; set; }
 
         public string CaracteristicaTelefono { get; set; }
         public string NumeroTelefono { get; set; }
