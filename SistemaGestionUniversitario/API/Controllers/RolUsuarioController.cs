48e43e6717e65355ee7f814fab770ffa264c0297
julianafailla <faillajuliana8@gmail.com>
2025-05-21 15:43:18 -0300

    getRoles

---
+++ SistemaGestionUniversitario/API/Controllers/RolUsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using Logica.Contracts;
+using Entidades.DTOs.Crear;
+using Entidades.DTOs.Modificar;
+using Entidades.DTOs.Respuestas;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RolUsuarioController : ControllerBase
+    {
+        private readonly IRolUsuarioLogic _rolUsuarioLogic;
+        public RolUsuarioController(IRolUsuarioLogic rolUsuarioLogic)
+        {
+            _rolUsuarioLogic = rolUsuarioLogic;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerRoles()
+        {
+            List<RolUsuarioDTO> rolUsuarioDTO = await _rolUsuarioLogic.ObtenerRoles();
+
+            return Ok(rolUsuarioDTO);
+        }
+    }
+}
