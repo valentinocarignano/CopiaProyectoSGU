2255f8351ffcb1bdefcee11a30a35179f7a53bc3
Valentino Carignano <valentino.carignano1@gmail.com>
2025-05-21 17:02:31 -0300

    controller notalaumno

---
+++ SistemaGestionUniversitario/API/Controllers/NotaAlumnoController.cs
@@ -0,0 +1,84 @@
+﻿using Entidades.DTOs.Crear;
+using Entidades.DTOs.Modificar;
+using Entidades.DTOs.Respuestas;
+using Logica.Contracts;
+using Logica.Implementations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotaAlumnoController : ControllerBase
+    {
+        private readonly INotaAlumnoLogic _notaAlumnoLogic;
+
+        public NotaAlumnoController(INotaAlumnoLogic notaAlumnoLogic)
+        {
+            _notaAlumnoLogic = notaAlumnoLogic;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ObtenerNotas()
+        {
+            List<NotaAlumnoDTO> notaAlumnoDTO = await _notaAlumnoLogic.ObtenerNotas();
+
+            return Ok(notaAlumnoDTO);
+        }
+
+        [HttpGet("id-materia/{id-materia}")]
+        public async Task<IActionResult> ObtenerNotasPorMateria(int idMateria)
+        {
+            List<NotaAlumnoDTO> notaAlumnoDTO = await _notaAlumnoLogic.ObtenerNotasPorMateria(idMateria);
+
+            return Ok(notaAlumnoDTO);
+        }
+
+        [HttpGet("id-alumno/{id-alumno}")]
+        public async Task<IActionResult> ObtenerNotasPorAlumno(int idAlumno)
+        {
+            List<NotaAlumnoDTO> notaAlumnoDTO = await _notaAlumnoLogic.ObtenerNotasPorAlumno(idAlumno);
+
+            return Ok(notaAlumnoDTO);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CrearNotaAlumno([FromBody] CrearNotaAlumnoDTO crearNotaAlumnoDTO)
+        {
+            await _notaAlumnoLogic.AltaNotaAlumno(crearNotaAlumnoDTO.Nota, crearNotaAlumnoDTO.IDAlumno, crearNotaAlumnoDTO.IDExamen);
+
+            return Ok();
+        }
+
+        [HttpPut("id-alumno/{id-alumno}/id-examen/{id-examen}")]
+        public async Task<IActionResult> ModificarNotaAlumno(int idAlumno, int idExamen, [FromBody] ModificarNotaAlumnoDTO modificarNotaAlumnoDTO)
+        {
+            NotaAlumnoDTO notaAlumnoDTO = await _notaAlumnoLogic.ActualizacionNotaAlumno(
+                idAlumno,
+                idExamen,
+                modificarNotaAlumnoDTO.Nota);
+
+            if (notaAlumnoDTO == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(notaAlumnoDTO);
+        }
+
+        [HttpDelete("id-alumno/{id-alumno}/id-examen/{id-examen}")]
+        public async Task<IActionResult> EliminarNotaAlumno(int idAlumno, int idExamen)
+        {
+            try
+            {
+                await _notaAlumnoLogic.BajaNotaAlumno(idAlumno, idExamen);
+
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ SistemaGestionUniversitario/Entidades/DTOs/Crear/CrearNotaAlumnoDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.DTOs.Crear
+{
+    public class CrearNotaAlumnoDTO
+    {
+        public int Nota { get; set; }
+        public int IDAlumno { get; set; }
+        public int IDExamen { get; set; }
+    }
+}
