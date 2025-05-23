e0ae81ac7cd75505ed6711cdc2db7c46e41b62cf
julianafailla <faillajuliana8@gmail.com>
2025-05-23 19:11:09 -0300

    gets

---
+++ SistemaGestionUniversitario/API/Controllers/DiaHorarioController.cs
@@ -0,0 +1,33 @@
+﻿using Logica.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiaHorarioController : ControllerBase
+    {
+        private readonly IDiaHorarioLogic _diaHorarioLogic;
+        public DiaHorarioController(IDiaHorarioLogic diaHorarioLogic)
+        {
+            _diaHorarioLogic = diaHorarioLogic;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var diaHorarios = await _diaHorarioLogic.ObtenerDiasHorarios();
+            return Ok(diaHorarios);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var diaHorario = await _diaHorarioLogic.ObtenerDiaHorarioID(id);
+            if (diaHorario == null)
+            {
+                return NotFound();
+            }
+            return Ok(diaHorario);
+        }
+    }
+}
