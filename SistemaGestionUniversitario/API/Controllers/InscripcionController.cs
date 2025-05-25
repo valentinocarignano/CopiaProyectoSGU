5bb1222a59355c6532041426b3ffd272bf30da92
Tomascinturion <tomascinturionices@gmail.com>
2025-05-25 20:01:44 -0300

    InscripcionControllerTerminado

---
+++ SistemaGestionUniversitario/API/Controllers/InscripcionController.cs
@@ -0,0 +1,61 @@
+﻿using Entidades.DTOs.Crear;
+using Entidades.DTOs.Respuestas;
+using Logica.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InscripcionController : ControllerBase
+    {
+        private readonly IInscripcionLogic _inscripcionLogic;
+        public InscripcionController(IInscripcionLogic inscripcionLogic)
+        {
+            _inscripcionLogic = inscripcionLogic;
+        }
+        [HttpGet]
+        public async Task<IActionResult> ObtenerInscripciones()
+        {
+            try
+            {
+                List<InscripcionDTO> inscripcionDTO = await _inscripcionLogic.ObtenerInscripciones();
+                return Ok(inscripcionDTO);
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+        [HttpPost]
+        public async Task<IActionResult> AltaInscripcion([FromBody]CrearInscripcionDTO crearInscripcionDTO)
+        {
+            try
+            {
+                await _inscripcionLogic.AltaInscripcion(crearInscripcionDTO.IdAlumno, crearInscripcionDTO.IdMateria);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+        [HttpDelete("{idMateria}/{idAlumno}")]
+        public async Task<IActionResult> BajaInscripcion(string idMateria, string idAlumno)
+        {
+            try
+            {
+                await _inscripcionLogic.BajaInscripcion(idMateria, idAlumno);
+                return Ok();
+            }
+            catch(Exception ex) 
+            {
+                return BadRequest(ex.Message);
+            }
+            
+        }
+    }
+}
---
+++ SistemaGestionUniversitario/Entidades/DTOs/Crear/CrearInscripcionDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.DTOs.Crear
+{
+    public class CrearInscripcionDTO
+    {
+        public string IdAlumno { get; set; }
+        public string IdMateria { get; set; }
+    }
+}
