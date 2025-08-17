84119ddae7502eebac297681fe65c080b7b26d38
Valentino Carignano <valentino.carignano1@gmail.com>
2025-08-17 18:31:34 -0300

    inscripcionController

--- SistemaGestionUniversitario/Entidades/DTOs/Respuestas/InscripcionDTO.cs
+++ SistemaGestionUniversitario/Entidades/DTOs/Respuestas/InscripcionDTO.cs
@@ -3,7 +3,13 @@
     public class InscripcionDTO
     {
         public int ID { get; set; }
+
         public int IdAlumno { get; set; }
+        public string NombreAlumno { get; set; }
+        public string ApellidoAlumno { get; set; }
+        public string DNIAlumno { get; set; }
+
         public int IdMateria { get; set; }
+        public string NombreMateria { get; set; }
     }
 }
